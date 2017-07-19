94cc5ea68ff03b8611150100c6d3e7acb6c1ccae
Alandryia Jones <jonesalandryia@gmail.com>
2017-07-19 14:36:51 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import moment from "moment";
+import "../styles/App.css";
+import earthquakes from "../data/earthquakes";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">EarthQuakes!!</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
